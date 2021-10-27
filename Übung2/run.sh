24b893a08a96aae16aff8e4365479f9b0f08dc28
npely <pelznik@gmail.com>
2021-10-27 10:47:40 +0200

    fixed typo

--- Übung2/run.sh
+++ Übung2/run.sh
@@ -4,4 +4,4 @@ java -jar antlr-4.9.2-complete.jar TimeSimple.g4
 echo "compiling the java file"
 javac -classpath ".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH" TimeSimple.java
 echo "run the program"
-java -classpath ".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig TimeSimple token -tokens < timeBeispiel.txt
+java -classpath ".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig TimeSimple tokens -tokens < timeBeispiel.txt
