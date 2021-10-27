1c95dd3b40199dfa5bfd6bbfc327aaa4b694100a
npely <pelznik@gmail.com>
2021-10-27 10:44:01 +0200

    added Linux bahs script

---
+++ Übung2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "run antlr4"
+java -jar antlr-4.9.2-complete.jar TimeSimple.g4
+echo "compiling the java file"
+javac -classpath ".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH" TimeSimple.java
+echo "run the program"
+java -classpath ".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig TimeSimple token -tokens < timeBeispiel.txt
