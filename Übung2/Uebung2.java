52422c14adbded46940e2b1658e85ac3f07897ad
Niklas Pelz <pelznik@gmail.com>
2022-02-08 20:05:15 +0100

    fixed the Regex for 2a

--- Übung2/Uebung2.java
+++ Übung2/Uebung2.java
@@ -1,17 +1,33 @@
-// RegexTest.java
+
+// Uebung2.java
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public final class Uebung2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         if (args.length != 1) {
-            System.err.println("java RegexTest <input>");
+            System.err.println("java Uebung2 <input>");
             System.exit(1);
         }
 
-        String regex = "(%[^dnocefisuxtXTSEgHhbBaA%]*[dnocefisuxtXTSEgHhbBaA%])|([a-z ]*)";
+        // Für den Text: "[a-z ]*";
 
-        Pattern pattern = Pattern.compile(regex);
+        // Für die Flags, optional, es können aber mehrere Flags hintereinander
+        // vorkommen:
+        // "[-#+ 0,(]*";
+
+        // Für den ArgumentIndex, optional, kann aber mehrmals
+        // vorkommen"([1-9][0-9]*\\$)*";
+
+        // Die Width, optional (einmal, oder garnicht): "(\\d*)?";
+
+        // Die Precision, optional (einmal, oder garnicht): "(\\.\\d*)?";
+
+        // Die verschiedenen Tokens für die Conversion:
+        // "[bBhHsScCdoxXeEfgGaAtT%n]";
+
+        Pattern pattern = Pattern
+                .compile("(%([1-9][0-9]*\\$)*[-#+ 0,(]*(\\d*)?(\\.\\d*)?[bBhHsScCdoxXeEfgGaAtT%n])|([a-z ]*)");
         Matcher matcher = pattern.matcher(args[0]);
 
         while (matcher.find() && !matcher.group().equals("")) {
